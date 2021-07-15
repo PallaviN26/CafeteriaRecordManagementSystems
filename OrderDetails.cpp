53410fd67ca9089c650ce1e961eb597f82c8ec07
PrathimaHolla <prathimaholla1606@gmail.com>
2021-07-15 22:38:26 +0530

    almost all errors corrected, cncel working but must make pack appending correct, have created validation method for item id validation so if anything I've missed use it..

--- OrderDetails.cpp
+++ OrderDetails.cpp
@@ -218,7 +218,7 @@ void orderDetails::deleteOrder(int id){
     while (!orderFile.eof())
     {
         pos=orderFile.tellg();
-        cout<<"Position "<<f<<" : "<<pos<<endl;
+        // cout<<"Position "<<f<<" : "<<pos<<endl;
         f++;
         unpack();
         if(id == orderId){
